b3121746715c130e083ad15d7c8bc7adde334262
Benni Dietz <b_diet05@uni-muenster.de>
2021-02-13 19:31:00 +0100

    Script automatically deriving deforestation/ forestation change

--- scripts/readLanduseData.R
+++ scripts/readLanduseData.R
@@ -13,4 +13,32 @@ imageOfYear = function(yearString) {
   return (raster(fileName))
 }
 
-raster2013 = imageOfYear("2013")
+cellCountOfRaster = function(raster, index) {
+  rasterValues = values(raster)
+  return ( length(subset(rasterValues, rasterValues == index)) )
+}
+
+areaOfRasterIndex = function(raster, index) {
+  return ( cellCountOfRaster(raster, index) * res(raster)^2 )
+}
+
+stringOutputCellCount = function(raster, index) {
+  count = cellCountOfRaster(raster, index)
+  percent = (count * 100 / length(raster[!is.na(raster)]))
+  return ( paste(count, " (",format(round(percent, 2), nsmall = 2) ,"%)", sep="") )
+}
+
+analyseChange = function(startYear, endYear) {
+  startForest = imageOfYear(startYear) == 3 # 1 if forest; 0 if not forest
+  endForest = imageOfYear(endYear) == 3 # 1 if forest; 0 if not forest
+  diffForestRaster = startForest - endForest
+  plot(diffForestRaster)
+  print(paste("Number of cells remainins the same:",
+              stringOutputCellCount(diffForestRaster, 0)))
+  print(paste("Number of cells for new forest:",
+              stringOutputCellCount(diffForestRaster, -1)))
+  print(paste("Number of cells deforested:",
+              stringOutputCellCount(diffForestRaster, 1)))
+}
+
+analyseChange("2006", "2016")
